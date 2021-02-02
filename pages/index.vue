d392946ab3e90247e82bd609827c6c5609c0bb31
uzdsmi-nf <uzdsmi-nf@protonmail.com>
2021-02-02 17:27:18 +0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mx-auto">
+    <h1>Lorem, ipsum, dolor. h1</h1>
+    <h2>Lorem, ipsum, dolor. h2</h2>
+    <h3>Lorem, ipsum, dolor. h3</h3>
+    <h4>Lorem, ipsum, dolor. h4</h4>
+    <p>Lorem, ipsum dolor, sit amet consectetur adipisicing elit. Accusantium nobis, soluta doloremque fugit beatae esse, ea mollitia in consectetur cum totam, voluptate officiis quam hic harum maiores earum. Error, non.</p>
+    <button>button</button>
+    <a href="/">Link</a>
+    <ul>
+      <li>UL > LI 1</li>
+      <li>UL > LI 2</li>
+    </ul>
+    <ol>
+      <li>OL > LI 1</li>
+      <li>OL > LI 2</li>
+    </ol>
+    <blockquote>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia fugiat porro iusto atque iste accusamus ratione sint, fuga accusantium obcaecati incidunt, earum autem reprehenderit quisquam distinctio, ducimus deleniti natus rerum.</p>
+    </blockquote>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
