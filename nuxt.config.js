9e606a2ccfd10bc18b8e657ed291e14b472b0a37
SharyZ <onshariiit@gmail.com>
2021-02-03 22:11:49 +0500

    remove fonts link

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,6 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter&family=Roboto:wght@400;500&display=swap' }
     ]
   },
 
