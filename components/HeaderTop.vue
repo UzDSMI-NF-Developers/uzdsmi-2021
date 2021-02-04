84b5a39af6409c6eddc3b16d50c2231ad15e6d11
uzdsmi-nf <uzdsmi-nf@protonmail.com>
2021-02-04 17:04:50 +0500

    add language switcher

--- components/HeaderTop.vue
+++ components/HeaderTop.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div class="py-2">
+    <LangSelect />
+  </div>
 </template>
+
+<script>
+import LangSelect from '@/components/LangSelect'
+
+export default {
+  components: {
+    LangSelect
+  }
+}
+</script>
---
+++ components/LangSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li>
+      <NuxtLink :to="switchLocalePath('en')">
+        <img src="/flags/flag-of-great-britain.svg" alt="Switch to English" />
+      </NuxtLink>
+    </li>
+    <li>
+      <NuxtLink :to="switchLocalePath('ru')">
+        <img src="/flags/flag-of-russia.svg" alt="Switch to Russia" />
+      </NuxtLink>
+    </li>
+    <li>
+      <NuxtLink :to="switchLocalePath('uz')">
+        <img src="/flags/flag-of-uzbekistan.svg" alt="Switch to Uzbek" />
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  ul {
+    @apply list-none flex items-center;
+  }
+
+  ul li {
+    @apply px-3;
+  }
+
+  ul img {
+    @apply w-8;
+  }
+</style>
