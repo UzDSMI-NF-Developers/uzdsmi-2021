bebbaecf34c2b01be5e4418bf8e31c515b327a81
uzdsmi-nf <uzdsmi-nf@protonmail.com>
2021-02-04 09:21:17 +0500

    remove function

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -17,7 +17,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
           </svg>
-          {{ postViewsCount }}
+          views
         </span>
       </div>
     </div>
@@ -32,11 +32,9 @@ export default {
     const { id } = params
     const { locale } = app.i18n
     const post = await $axios.$get(`https://admin.uzdsmi-nf.uz/wp-json/wp/v2/posts/${id}`)
-    const postViewsCount = await $axios.$get(`https://admin.uzdsmi-nf.uz/wp-json/base/views/${id}`)
 
     return {
       post,
-      postViewsCount
     }
   },
   head() {
