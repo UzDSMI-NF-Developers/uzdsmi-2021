c66445597bbb792c67e74a099b4312e75f2acfc3
SharyZ <onshariiit@gmail.com>
2021-02-05 22:31:21 +0500

    add pagination to news page

---
+++ components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="flex">
+    <button
+      v-if="pagination.prevPage"
+      @click="$emit('prev')"
+      class="ml-4 mr-4"
+    >
+      <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+      </svg>
+    </button>
+    <button v-else class="ml-4 mr-4 disabled:opacity-50 cursor-not-allowed" disabled>
+      <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+      </svg>
+    </button>
+
+    <button
+      v-if="pagination.nextPage"
+      @click="$emit('next')"
+    >
+      <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+      </svg>
+    </button>
+    <button v-else class="disabled:opacity-50 cursor-not-allowed" disabled>
+      <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+      </svg>
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['pagination'],
+}
+</script>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -3,7 +3,10 @@
     <Heading :title="$t('news.all')"></Heading>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-6 mb-4">
       <!-- card -->
-      <div v-for="post in news" class="bg-white dark:bg-gray-600 shadow hover:shadow-lg transition border border-width-2 border-gray-200 rounded flex flex-col justify-between">
+      <div v-if="loading">
+        <Loading />
+      </div>
+      <div v-else v-for="post in news" class="bg-white dark:bg-gray-600 shadow hover:shadow-lg transition border border-width-2 border-gray-200 rounded flex flex-col justify-between">
         <figure class="h-60">
           <NuxtLink :to="localePath('/news/' + post.id)">
             <img :src="post._embedded['wp:featuredmedia']['0'].source_url" alt="" class="h-full mx-auto" />
@@ -20,7 +23,7 @@
             <svg class="mr-1 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            {{ $dateFns.format(post.date, 'eeee, d MMMM, y', { locale: locale === 'en' ? 'en-US' : locale }) }}
+            {{ $dateFns.format(post.date, 'eeee, d MMMM, y', { locale }) }}
           </span>
           <span class="text-gray-800 dark:text-gray-200 text-sm font-semibold flex items-center">
             <svg class="mr-1 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -32,26 +35,99 @@
         </div>
       </div>
     </div>
+    <div class="flex items-center justify-center pt-8 pb-4">
+      <code class="ml-4 rounded-lg bg-gray-200 p-2">
+        {{ $t('page') }}:
+        <input
+          type="number"
+          min="1"
+          :max="this.pagination.totalPages"
+          v-model="postsData.page"
+          @change="jumpTo($event.target.value)"
+        />
+      </code>
+      <Pagination :pagination="pagination"
+        @prev="--postsData.page; getPosts();"
+        @next="postsData.page++; getPosts();">
+      </Pagination>
+    </div>
   </Container>
 </template>
 
 <script>
 import Container from '@/components/Container'
 import Heading from '@/components/Heading'
+import Pagination from '@/components/Pagination'
+import Loading from '@/components/Loading'
 
 export default {
-  async asyncData({ $axios, app }) {
-    const { locale } = app.i18n
-    const news = await $axios.$get(`https://admin.uzdsmi-nf.uz/wp-json/wp/v2/posts?categories=2&per_page=12&_embed`)
-
-    return {
-      news,
-      locale
-    }
-  },
   components: {
     Container,
-    Heading
+    Heading,
+    Pagination,
+    Loading
+  },
+  data() {
+    return {
+      newsUrl: `https://admin.uzdsmi-nf.uz/wp-json/wp/v2/posts?categories=2&_embed`,
+      news: [],
+      loading: true,
+      postsData: {
+        per_page: 10,
+        page: 1
+      },
+      pagination: {
+        prevPage: '',
+        nextPage: '',
+        totalPages: '',
+        from: '',
+        to: '',
+        total: ''
+      }
+    }
+  },
+  mounted() {
+    this.getPosts()
+  },
+  methods: {
+    getPosts() {
+      this.scrollToTop()
+      this.loading = true
+      this.$axios.get(this.newsUrl, { params: this.postsData }).then((response) => {
+        this.news = response.data
+        this.configPagination(response.headers)
+        this.loading = false
+      }).catch((error) => {
+        console.log(error)
+        this.loading = false
+      })
+    },
+    configPagination(headers) {
+      this.pagination.total = +headers['x-wp-total'];
+      this.pagination.totalPages = +headers['x-wp-totalpages'];
+      this.pagination.from = this.pagination.total ? ((this.postsData.page - 1) * this.postsData.per_page) + 1 : ' ';
+      this.pagination.to = (this.postsData.page * this.postsData.per_page) > this.pagination.total ? this.pagination.total : this.postsData.page * this.postsData.per_page;
+      this.pagination.prevPage = this.postsData.page > 1 ? this.postsData.page : '';
+      this.pagination.nextPage = this.postsData.page < this.pagination.totalPages ? this.postsData.page + 1 : '';
+    },
+    scrollToTop() {
+      window.scrollTo(0, 0)
+    },
+    jumpTo(page) {
+      if (page > this.pagination.totalPages || page <= 0) {
+        return
+      } else {
+        this.postsData.page = page
+        this.getPosts()
+      }
+    }
+  },
+  computed: {
+    locale() {
+      const currentLocale = this.$i18n.locale
+
+      return currentLocale === 'en' ? 'en-US' : currentLocale
+    }
   },
   layout: 'page',
 }
