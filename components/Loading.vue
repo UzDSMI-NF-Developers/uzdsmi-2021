585c817f49e6f906740e800bb284342014505748
SharyZ <onshariiit@gmail.com>
2021-02-05 22:31:07 +0500

    create Loading component

---
+++ components/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 ">
+    <div style="border-top-color:transparent" class="border-solid animate-spin rounded-full border-blue-500 border-8 h-8 w-8"></div>
+  </div>
+</template>
