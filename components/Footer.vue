a51ade70924a278a5ef9deafb9cc60b1199f9ece
uzdsmi-nf <uzdsmi-nf@protonmail.com>
2021-02-06 14:36:33 +0500

    remove columns

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,103 +3,37 @@
     <Container
       class="px-5 py-12 flex md:items-center lg:items-start md:flex-row md:flex-no-wrap flex-wrap flex-col"
     >
-      <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
+      <div class="w-full md:w-1/2 text-center">
+        <h3 class="font-medium tracking-widest mb-3 uppercase">
+          {{ $t('information') }}
+        </h3>
+        <ul class="list-none mb-10 space-y-3">
+          <li>
+            <strong class="text-md">
+              {{ $t('site_title') }}
+            </strong>
+          </li>
+          <li>
+            <span>
+              <strong>{{ $t('address.title') }}</strong>
+              :
+              <em>{{ $t('address.place') }}</em>
+            </span>
+          </li>
+          <li>
+            <span>
+              <strong>{{ $t('phone') }}</strong>
+              :
+              <em>(998)-61-224-29-02</em>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/2 mb-10">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d966.8707287551921!2d59.579297429243034!3d42.454690788181146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41dd99dd0afc7dbd%3A0x38ac8257fbb9aaff!2z0J3Rg9C60YPRgdGB0LrQuNC5INGE0LjQu9C40LDQuyDQk9C-0YHRg9C00LDRgNGB0YLQstC10L3QvdC-0LPQviDQuNC90YHRgtC40YLRg9GC0LAg0LjRgdC60YPRgdGB0YLQstCwINC4INC60YPQu9GM0YLRg9GA0Ysg0KPQt9Cx0LXQutC40YHRgtCw0L3QsA!5e1!3m2!1sru!2s!4v1602133969678!5m2!1sru!2s"
-          class="border border-gray-500 p-1 rounded-lg"
+          class="w-full h-72 border border-gray-500 p-1 rounded-lg"
         ></iframe>
-      </div>
-      <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
-        <div class="lg:w-1/3 md:w-1/2 w-full px-4">
-          <h3 class="font-medium tracking-widest mb-3 uppercase">
-            {{ $t('categories') }}
-          </h3>
-          <ul class="list-none mb-10 space-y-3">
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                {{ $t('nav.branch.title') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                {{ $t('nav.activity.title') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                {{ $t('nav.applicants.title') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                {{ $t('nav.students.title') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                {{ $t('nav.interactive_services.title') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                {{ $t('nav.normative_legal_documents.title') }}
-              </NuxtLink>
-            </li>
-          </ul>
-        </div>
-        <div class="lg:w-1/3 md:w-1/2 w-full px-4">
-          <h3 class="font-medium tracking-widest mb-3 uppercase">
-            {{ $t('news.latest') }}
-          </h3>
-          <ul class="list-none mb-10 space-y-3">
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                First Link
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                Second Link
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                Third Link
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink :to="localePath('/')" class="text-gray-800 dark:text-gray-200 hover:text-black">
-                Fourth Link
-              </NuxtLink>
-            </li>
-          </ul>
-        </div>
-        <div class="lg:w-1/3 md:w-1/2 w-full px-4">
-          <h3 class="font-medium tracking-widest mb-3 uppercase">
-            {{ $t('information') }}
-          </h3>
-          <ul class="list-none mb-10 space-y-3">
-            <li>
-              <strong class="text-md">
-                {{ $t('site_title') }}
-              </strong>
-            </li>
-            <li>
-              <span>
-                <strong>{{ $t('address.title') }}</strong>
-                :
-                <em>{{ $t('address.place') }}</em>
-              </span>
-            </li>
-            <li>
-              <span>
-                <strong>{{ $t('phone') }}</strong>
-                :
-                <em>(998)-61-224-29-02</em>
-              </span>
-            </li>
-          </ul>
-        </div>
       </div>
     </Container>
     <div class="bg-gray-200 dark:bg-gray-800">
