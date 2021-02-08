c7e150a4ebe601b263db3e702f388965d6a40f24
uzdsmi-nf <uzdsmi-nf@protonmail.com>
2021-02-08 10:35:13 +0500

    add link

--- components/HeaderTop.vue
+++ components/HeaderTop.vue
@@ -63,9 +63,15 @@
       <ul class="flex items-center justify-between mb-4">
         <li class="group bg-gray-200 py-2 px-4 rounded-l flex items-center">
           <svg class="group-hover:text-blue-700 w-6 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+          </svg>
+          <a href="http://edu.uz/uz/pages/ilm-fan-2020e" target="_blank" rel="nofollow noopener" class="group-hover:text-blue-700 text-black font-semibold text-xs hover:no-underline">Ilm-Fan 2020</a>
+        </li>
+        <li class="group bg-gray-200 py-2 px-4 border-r-2 border-l-2 flex items-center">
+          <svg class="group-hover:text-blue-700 w-6 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-          <NuxtLink :to="localePath('/online-reception')" class="group-hover:text-blue-700 text-black font-semibold text-sm hover:no-underline">
+          <NuxtLink :to="localePath('/online-reception')" class="group-hover:text-blue-700 text-black font-semibold text-xs hover:no-underline">
             Rektor qabulxonasi
           </NuxtLink>
         </li>
@@ -73,7 +79,7 @@
           <svg class="group-hover:text-blue-700 w-6 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
           </svg>
-          <NuxtLink :to="localePath('/')" class="group-hover:text-blue-700 text-black font-semibold text-sm hover:no-underline">
+          <NuxtLink :to="localePath('/')" class="group-hover:text-blue-700 text-black font-semibold text-xs hover:no-underline">
             Elektron kutubxona
           </NuxtLink>
         </li>
@@ -81,7 +87,7 @@
           <svg class="group-hover:text-blue-700 w-6 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
           </svg>
-          <NuxtLink :to="localePath('/')" class="group-hover:text-blue-700 text-black font-semibold text-sm hover:no-underline">
+          <NuxtLink :to="localePath('/')" class="group-hover:text-blue-700 text-black font-semibold text-xs hover:no-underline">
             Online kameralar
           </NuxtLink>
         </li>
